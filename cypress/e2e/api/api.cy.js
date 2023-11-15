6e64139edf2c23ffe4562389a887b076e00535c0
rafaelsuzanocruz <150152869+rafaelsuzanocruz@users.noreply.github.com>
2023-11-15 17:51:40 -0300

    novos cenarios

--- cypress/e2e/api/api.cy.js
+++ cypress/e2e/api/api.cy.js
@@ -3,119 +3,186 @@ let dt_login1
 let dt_login
 let AccessToken
 
-describe('Test API',() =>{
+describe('Test API', () => {
 
-    before(() => {
-        cy.fixture('login.json').then(dt_login => {
-            dt_login1 = dt_login;
-        })
+  before(() => {
+    cy.fixture('login.json').then(dt_login => {
+      dt_login1 = dt_login;
     })
-  
-    it('Health Test',()=> {
-        cy.request({
-        method: 'GET',
-        url: url  +'health',
-        form:true
+  })
+
+  it('Health Test', () => {
+    cy.api({
+      method: 'GET',
+      url: url + 'health',
+      form: true
 
 
-        }).then(Response =>{
-            expect(Response.status).to.eq(200)
-            cy.log(JSON.stringify(Response.body))
+    }).then(Response => {
+      expect(Response.status).to.eq(200)
+      cy.log(JSON.stringify(Response.body))
 
 
-        })
     })
+  })
 
 
-    it('Login Test NOK',()=> {
-        cy.request({
-        method: 'POST',
-        url: url  +'login',
+  it('Login Test NOK', () => {
+    cy.api({
+      method: 'POST',
+      url: url + 'login',
 
-        body: dt_login1['Login_NOK'],  
-          headers :{
-           
-            'accept': 'text/plain',
-            'Content-Type': 'application/json',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0',
-          },
-          
-          
-          failOnStatusCode:false,
-        }).then(Response =>{
-            cy.log(JSON.stringify(Response.body))
-            expect(Response.status).to.eq(401)
-           
+      body: dt_login1['Login_NOK'],
+      headers: {
+
+        'accept': 'text/plain',
+        'Content-Type': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0',
+      },
 
 
-        })
+      failOnStatusCode: false,
+    }).then(Response => {
+      cy.log(JSON.stringify(Response.body))
+      expect(Response.status).to.eq(401)
+
+
+
     })
+  })
 
-    it('Login Test OK',()=> {
-        cy.request({
-        method: 'POST',
-        url: url  +'login',
+  it('Login Test OK', () => {
+    cy.api({
+      method: 'POST',
+      url: url + 'login',
 
-        body: dt_login1['Login_OK'],  
-          headers :{
-           
-            'accept': 'text/plain',
-            'Content-Type': 'application/json',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0',
-          },
-          
-          
-          failOnStatusCode:false,
-        }).then(Response =>{
-            cy.log(JSON.stringify(Response.body))
+      body: dt_login1['Login_OK'],
+      headers: {
+
+        'accept': 'text/plain',
+        'Content-Type': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0',
+      },
+
+      failOnStatusCode: false,
+    }).then(Response => {
+      cy.log(JSON.stringify(Response.body))
 
 
-            AccessToken = ((Response.body.accessToken))
-            expect(Response.status).to.eq(200)
-           
+      AccessToken = ((Response.body.accessToken))
+      expect(Response.status).to.eq(200)
 
-        })
+
     })
+  })
 
-    it('Change Password OK',()=> {
-        cy.request({
-        method: 'POST',
-        url: url  +'change-password',
-
-        body: 
-
-    {
+  it('Change Password OK', () => {
+    cy.request({
+      method: 'POST',
+      url: url + 'change-password',
+      body:
+      {
         "oldPassword": "Suz@no30",
         "newPassword": "Suz@no30",
         "accessToken": AccessToken,
         "role": "hbb"
+      }
+      ,
+      headers: {
+        'accept': 'text/plain',
+        'Content-Type': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0',
+      },
+      failOnStatusCode: false,
+    }).then(Response => {
+      cy.log(JSON.stringify(Response.body))
+      cy.log(AccessToken)
 
-        }
-        
-        
-        
-        
-        ,  
-          headers :{
-           
-            'accept': 'text/plain',
-            'Content-Type': 'application/json',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0',
-          },
-          
-          
-          failOnStatusCode:false,
-        }).then(Response =>{
-            cy.log(JSON.stringify(Response.body))
-            cy.log(AccessToken)
-         
-            //expect(Response.status).to.eq(200)
-           
-
-
-        })
     })
+  })
+
+
+  it('Forgot Password', () => {
+    cy.request({
+      method: 'POST',
+      url: url + 'forgot-password',
+      body:
+      {
+        "email": "rafaelsuzano@penzack.com",
+        "role": "hbb",
+
+      }
+      ,
+      headers: {
+        'accept': 'text/plain',
+        'Content-Type': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0',
+      },
+      failOnStatusCode: false,
+    }).then(Response => {
+      cy.log(JSON.stringify(Response.body))
+  
+      expect(Response.status).to.eq(200)
+
+    })
+  })
+
+
+  it('Confirm Forgot Password', () => {
+    cy.request({
+      method: 'POST',
+      url: url + 'confirm-forgot-password',
+      body:
+      {
+               "user": "rafaelsuzano@penzack.com",
+          "code":"748347",
+          "password":"Suz@no30",
+          "role":"hbb"
+    
+      }
+      ,
+      headers: {
+        'accept': 'text/plain',
+        'Content-Type': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0',
+      },
+      failOnStatusCode: false,
+    }).then(Response => {
+      cy.log(JSON.stringify(Response.body))
+  
+      //expect(Response.status).to.eq(200)
+
+    })
+  })
+
+
+  it('Resend Email Confirmation', () => {
+    cy.request({
+      method: 'POST',
+      url: url + 'resend-email-confirmation',
+      body:
+      {
+               "email": "rafaelsuzano@penzack.com",
+                 "role":"hbb"
+    
+      }
+      ,
+      headers: {
+        'accept': 'text/plain',
+        'Content-Type': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0',
+      },
+      failOnStatusCode: false,
+    }).then(Response => {
+      cy.log(JSON.stringify(Response.body))
+      expect(Response.status).to.eq(200)
+
+
+    })
+  })
+
+
+
+
 
 })
-
-
